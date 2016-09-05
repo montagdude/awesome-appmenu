38020c7cdf61fd11f3b9f9f25e43ed344c708327
Daniel Prosser <dpross1100@msn.com>
2016-09-04 23:25:47 -0400

    Initial commit

---
+++ menurc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+
+home = os.environ["HOME"]
+
+""" List of paths to find launchers and icons, given in order of priority. They
+    are searched recursively, so there's no need to enter subpaths. """
+launcherpaths=["/usr/share/applications", home + "/.local/share/applications"]
+iconpaths=["/usr/share/icons/Adwaita", "/usr/share/icons/oxygen", "/usr/share/icons/hicolor"]
+
+""" List of categories. Each inner list stores Category name in .desktop file,
+    category name displayed in menu, and icon generic name (in that order) """
+categories = [ ["Utility",     "Accessories", "applications-utilities"],
+               ["Development", "Development", "applications-development"],
+               ["Education",   "Education",   "applications-science"],
+               ["Game",        "Games",       "applications-games"],
+               ["Graphics",    "Graphics",    "applications-graphics"],
+               ["Network",     "Internet",    "applications-internet"],
+               ["Office",      "Office",      "applications-office"],
+               ["AudioVideo",  "MultiMedia",  "applications-multimedia"],
+               ["Settings",    "Settings",    "applications-accessories"],
+               ["System",      "System",      "applications-system"],
+               ["Wine",        "Wine",        "wine"] ]
